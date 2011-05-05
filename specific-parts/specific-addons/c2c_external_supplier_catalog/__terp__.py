0793086c45a6902af6e5e3b4737893c4a78fe94b
Guewen Baconnier @ CampToCamp <guewen.baconnier@camptocamp.com>
2011-05-05 09:59:16 +0200

    [ADD] moved c2c_external_supplier_catalog from extra-addons

---
+++ specific-parts/specific-addons/c2c_external_supplier_catalog/__terp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Guewen Baconnier. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'c2c_external_supplier_catalog',
+    'version' : '1.0',
+    'depends' : ['base',
+                 'product',
+                 ],
+    'author' : 'Camptocamp',
+    'description': """Mapping between external categories and OpenERP categories per supplier.
+Coefficient on each external category / product.
+Sale price is computed as purchase price * product's coefficient.
+
+Designed only to be used with Kettle to import / update products.
+
+If a product does not exists, its coefficient is determined from the external category.
+Otherwise, its coefficient is never updated.
+""",
+    'website': 'http://www.camptocamp.com',
+    'init_xml': [],
+    'update_xml': ['product_view.xml',
+                   'supplier_catalog_category_view.xml',
+                  ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
---
+++ specific-parts/specific-addons/c2c_external_supplier_catalog/supplier_catalog_category.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Guewen Baconnier. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+#----------------------------------------------------------
+# Supplier Catalog Categories
+#----------------------------------------------------------
+class supplier_catalog_category(osv.osv):
+    _name = 'supplier.catalog.category'
+    _description = "Supplier Catalog Categories"
+
+    _columns = {
+        'supplier_code': fields.selection((('toolstream', 'Toolstream'),), 'Supplier Code', required=True),
+        'name': fields.char('Supplier\'s Category Code', size=30, required=True),
+        'category_id': fields.many2one('product.category', 'Category', required=True),
+        'price_coefficient': fields.float('Price Coefficient', digits=(3,2), required=True),
+    }
+
+    _defaults = {
+        'price_coefficient':lambda *a: 1.0,
+    }
+
+supplier_catalog_category()
+
+#----------------------------------------------------------
+# Product Categories
+#----------------------------------------------------------
+class product_category(osv.osv):
+    _inherit = 'product.category'
+
+    _columns = {
+                'supplier_catalog_category_ids': fields.one2many('supplier.catalog.category', 'category_id', 'Supplier Catalog Categories'),
+                }
+
+product_category()
