850f9a7d7617bc3319ba697f8855c1907cd7157b
Guewen Baconnier @ CampToCamp <guewen.baconnier@camptocamp.com>
2011-05-24 11:54:03 +0200

    [ADD] c2c_magento_product_gift

---
+++ specific-parts/specific-addons/c2c_magento_product_gift/__terp__.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Guewen Baconnier. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'Magento Product Gifts',
+    'version' : '1.0',
+    'depends' : ['base', 'base_sale_multichannels', 'magentoerpconnect'],
+    'author' : 'Camptocamp',
+    'description': """ Create product links of type "Gift".
+Needs Magento module : name_of_module
+Find it at : url
+    """,
+    'website': 'http://www.camptocamp.com',
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
---
+++ specific-parts/specific-addons/c2c_magento_product_gift/product_links.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Guewen Baconnier. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+from tools.translate import _
+import netsvc
+
+
+class ProductLink(osv.osv):
+    _inherit = 'product.link'
+
+    def get_link_type_selection(self, cr, uid, context=None):
+        res = super(ProductLink, self).get_link_type_selection(cr, uid, context)
+        if not 'gift' in [key[0] for key in res]:
+            res.append(('gift', 'Gift'))
+        return res
+
+    _columns = {
+        'type': fields.selection(get_link_type_selection, 'Link type', required=True)
+    }
+
+ProductLink()
