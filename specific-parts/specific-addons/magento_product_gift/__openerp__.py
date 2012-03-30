71d315accca2fdcc357ca84ed113d75246b7476c
unknown <nicolas.bessi@camptocamp.com>
2012-03-30 11:30:28 +0200

    [ADD] port of magento_product_gift

--- specific-parts/specific-addons/magento_product_gift/__openerp__.py
+++ specific-parts/specific-addons/magento_product_gift/__openerp__.py
@@ -1,35 +1,41 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Author Guewen Baconnier. Copyright Camptocamp SA
+#    Copyright 2012 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    'name' : 'Magento Product Gifts',
-    'version' : '1.0',
-    'depends' : ['base', 'product_links', 'magentoerpconnect'],
-    'author' : 'Camptocamp',
-    'description': """ Create product links of type "Gift".
+{'name' : 'Magento product gift',
+ 'version' : '1.2',
+ 'author' : 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'category': 'Others',
+ 'complexity': "normal",  # easy, normal, expert
+ 'depends' : ['base', 'product_links', 'magentoerpconnect'],
+ 'description': """ Create product links of type "Gift".
 Needs Magento module : name_of_module
-Find it at : url
-    """,
-    'website': 'http://www.camptocamp.com',
-    'init_xml': [],
-    'update_xml': [],
-    'demo_xml': [],
-    'installable': True,
-    'active': False,
-}
+Find it at : url""",
+ 'website': 'http://www.camptocamp.com',
+ 'init_xml': [],
+ 'update_xml': [],
+ 'demo_xml': [],
+ 'tests': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'license': 'AGPL-3',
+ 'application': True,}
+
+
--- specific-parts/specific-addons/magento_product_gift/product_links.py
+++ specific-parts/specific-addons/magento_product_gift/product_links.py
@@ -1,29 +1,27 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Author Guewen Baconnier. Copyright Camptocamp SA
+#    Copyright 2012 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from osv import fields, osv
-from tools.translate import _
-import netsvc
+from openerp.osv.orm import Model, fields
 
-
-class ProductLink(osv.osv):
+class ProductLink(Model):
     _inherit = 'product.link'
 
     def get_link_type_selection(self, cr, uid, context=None):
@@ -41,4 +39,3 @@ class ProductLink(osv.osv):
         'sequence': lambda *a: 1,
     }
 
-ProductLink()
