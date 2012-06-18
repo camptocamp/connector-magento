67e889a235a7adb520d996a2ebd4247115be5dc0
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2012-06-18 14:38:19 +0200

    [ADD] reconciliation method with tid_ + ref tid_mag_ + ref because debonix has many move lines with a ref prefixed

---
+++ specific-parts/specific-addons/account_tid_reconcile/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2012 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': "Advanced Reconcile with TID",
+ 'version': '1.0',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'category': 'Finance',
+ 'complexity': 'normal',
+ 'depends': ['account_advanced_reconcile'],
+ 'description': """
+ Add specific reconciliation method which searches by :
+ On: credit     => debit
+     partner_id => partner_id
+     ref        => name or ref or "tid_" + ref or "tid_mag_" + ref
+
+ """,
+ 'website': 'http://www.camptocamp.com',
+ 'init_xml': [],
+ 'update_xml': [],
+ 'demo_xml': [],
+ 'test': [],
+ 'images': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'license': 'AGPL-3',
+}
