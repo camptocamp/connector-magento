ed7383c2c2152ceec21747c5e90fb4d056a5b0c9
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-05-14 17:00:23 +0200

    migrated sale order report

--- specific-parts/specific-addons/specific_report/report/sale_order.py
+++ specific-parts/specific-addons/specific_report/report/sale_order.py
@@ -22,45 +22,44 @@
 
 import time
 import datetime
-from report import report_sxw
-from osv import osv
+from openerp.report import report_sxw
+
 
 class Order(report_sxw.rml_parse):
     _name = 'report.sale.order_custom'
-    
+
     def __init__(self, cr, uid, name, context):
         super(Order, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
             'estimated_delivery_date': self._get_estimated_delivery_date,
-            'hide_partner_name': self._hide_partner_name,
+            'show_discount': self._show_discount,
         })
-        
+
     def _get_estimated_delivery_date(self, order_obj):
-        """Compute the estimated delivery date as : take the highest delivery lead time in order lines and add it to the order date """
+        """Compute the estimated delivery date as : take the highest
+        delivery lead time in order lines and add it to the order date
+        """
         max_delay = 0
         for line in order_obj.order_line:
             if line.delay > max_delay:
                 max_delay = line.delay
-            
-        max_date = datetime.date.fromtimestamp(time.mktime(time.strptime(order_obj.date_order, "%Y-%m-%d")))
+
+        max_date = datetime.date.fromtimestamp(
+            time.mktime(time.strptime(order_obj.date_order, "%Y-%m-%d")))
         max_date = max_date + datetime.timedelta(days=max_delay)
 
         return max_date
-            
-    def _hide_partner_name(self, partner_obj):
-        """ Define if the partner name must be displayed according to the
-        list configured on the company.
-        Return true if name must be hidden
-        """
-        user = self.pool.get('res.users').browse(self.cr, self.uid, self.uid)
-        company = user.company_id
-        if partner_obj.title and partner_obj.title in [x.shortcut for x in company.report_hide_partner_title_ids]:
-            return True
-        return False
-        
-        
-report_sxw.report_sxw('report.sale.order_custom','sale.order','addons/specific_report/report/sale_order.rml',parser=Order)
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    def _show_discount(self, uid, context=None):
+        cr = self.cr
+        try: 
+            group_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'sale', 'group_discount_per_so_line')[1]
+        except:
+            return False
+        return group_id in [x.id for x in self.pool.get('res.users').browse(cr, uid, uid, context=context).groups_id]
 
+report_sxw.report_sxw('report.sale.order_custom',
+                      'sale.order',
+                      'addons/specific_report/report/sale_order.rml',
+                      parser=Order)
