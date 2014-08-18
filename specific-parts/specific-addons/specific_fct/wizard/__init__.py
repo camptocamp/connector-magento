42399e915a0f36eb48a1a6c6f21849a80d0fb3f3
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-08-18 18:07:41 +0200

    They want to be able to close periods even when period contain draft moves
    because they use the state as a security

--- specific-parts/specific-addons/specific_fct/wizard/__init__.py
+++ specific-parts/specific-addons/specific_fct/wizard/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import base_partner_merge
+from . import account_period_close
---
+++ specific-parts/specific-addons/specific_fct/wizard/account_period_close.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+class account_period_close(orm.TransientModel):
+    _inherit = 'account.period.close'
+
+    def data_save(self, cr, uid, ids, context=None):
+        """
+        Override the method not to block if the period contains draft
+        move lines
+        """
+        period_pool = self.pool.get('account.period')
+        account_move_obj = self.pool.get('account.move')
+
+        mode = 'done'
+        for form in self.read(cr, uid, ids, context=context):
+            if form['sure']:
+                for id in context['active_ids']:
+                    cr.execute('update account_journal_period set '
+                               'state=%s where period_id=%s',
+                               (mode, id))
+                    cr.execute('update account_period set state=%s '
+                               'where id=%s', (mode, id))
+
+        return {'type': 'ir.actions.act_window_close'}
