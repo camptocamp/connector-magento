cba9ad2c58d6fdd77c9aedc01773a04483676fc6
vrenaville <vincent.renaville@camptocamp.com>
2015-01-16 09:58:34 +0100

    [FIX] pep8

--- specific-parts/specific-addons/specific_fct/account_invoice.py
+++ specific-parts/specific-addons/specific_fct/account_invoice.py
@@ -65,14 +65,14 @@ class account_invoice(orm.Model):
                                          context=context)
             content = StringIO(result)
             filename = "%s-%s.%s" % (sale.name, invoice.number, ext)
-            ## Load private key for transfert
+            # Load private key for transfert
             privatekeyfile = os.path.expanduser('~/.ssh/id_rsa')
             mykey = paramiko.RSAKey.from_private_key_file(privatekeyfile)
             transport = paramiko.Transport((company.sftp_invoice_host,
                                             company.sftp_invoice_port))
             try:
                 transport.connect(username=company.sftp_invoice_user,
-                                  pkey = mykey)
+                                  pkey=mykey)
                 client = paramiko.SFTPClient.from_transport(transport)
                 try:
                     client.chdir(company.sftp_invoice_path)
