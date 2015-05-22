c982b50dc05871696a03acc5561540b389a7e4a5
Charbel Jacquin <charbel.jacquin@camptocamp.com>
2015-05-22 09:41:47 +0200

    noise reduction after code review

--- specific-parts/specific-addons/debonix_purchase_edi/purchase_edi.py
+++ specific-parts/specific-addons/debonix_purchase_edi/purchase_edi.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+w# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Author: Charbel Jacquin (Camptocamp)
@@ -25,9 +25,18 @@ import os
 import logging
 import simplejson
 
+import tools.edifact
+
 _logger = logging.getLogger('EDIFACT')
 _logger.setLevel(logging.DEBUG)
 _logger.debug('loading module !')
+
+
+def _gv(obj, attr):
+    if not obj:
+        return ''
+    val = getattr(obj, attr)
+    return val if val else ''
 
 
 class purchase_order(osv.Model):
@@ -68,44 +77,37 @@ If you need to regenerate, please ask your DBA to clear the 'edifact_sent' statu
 
     def _build_mapping(self, order):
         """ generate data mapping needed for template rendering """
-        mapping = {}
 
-        mapping['codefiliale'] = 'PLN'
-        mapping['siretFiliale'] = '52789590800012'
-        mapping['dateCmd'] = order.date_order.replace('-', '')
-        heure = order.create_date.split()[1].replace(':', '')
-        mapping['heureCmd'] = heure
-        # mapping['heureCmd'] = '090420'
+        dest_address = order.dest_address_id
+        def convert_unit(name):
+            if name == 'PCE':
+                return 'P'
+            assert 0 == 1
 
-        mapping['codeAgence'] = '928'
-        mapping['siretAgence'] = '52789590800012'
-        mapping['siretDebonix'] = '49039922700035'
-
-        mapping['noCmd'] = order.name
-        mapping['refCmd'] = order.name
-        mapping['dateLiv'] = order.minimum_planned_date.replace('-', '')
-        mapping['compteDebonix'] = '0000175000'
-
-        mapping['adr1'] = order.dest_address_id.name
-        mapping['adr2'] = ''
-        mapping['adr3'] = order.dest_address_id.street
-        mapping['adr4'] = ''
-        mapping['adr5'] = ''
-        mapping['cp'] = order.dest_address_id.zip
-        mapping['ville'] = order.dest_address_id.city
-        mapping['tel'] = order.dest_address_id.phone
-
-        fax = order.dest_address_id.fax
-        if fax:
-            mapping['fax'] = fax
-        else:
-            mapping['fax'] = ''
-        # add160 Paramètres : commentaire
-        # if($donnees->notes) $cmd->add160(utf8_decode($donnees->notes));
-        if order.notes:
-            mapping['commentaire'] = order.notes
-        else:
-            mapping['commentaire'] = ''
+        mapping = {
+            'codefiliale': 'PLN',
+            'siretFiliale': '52789590800012',
+            'dateCmd': _gv(order, 'date_order').replace('-', ''),
+            'heureCmd': _gv(order, 'create_date').split()[1].replace(':', ''),
+            'codeAgence': '928',
+            'siretAgence': '52789590800012',
+            'siretDebonix': '49039922700035',
+            'noCmd': _gv(order, 'name'),
+            'refCmd': _gv(order, 'name'),
+            'dateLiv': _gv(order, 'minimum_planned_date').replace('-', ''),
+            'compteDebonix': '0000175000',
+            # NEED REVIEW adress mapping. edifact 5*35, oerp 2*128
+            'adr1': _gv(dest_address, 'name'),
+            'adr2': '',
+            'adr3': _gv(dest_address, 'street'),
+            'adr4': '',
+            'adr5': '',
+            'cp': _gv(dest_address, 'zip'),
+            'ville': _gv(dest_address, 'city'),
+            'tel': _gv(dest_address, 'phone'),
+            'fax': _gv(dest_address, 'fax'),
+            'commentaire': _gv(order, 'notes')
+        }
 
         order_lines = list(order.order_line)
 
@@ -114,8 +116,22 @@ If you need to regenerate, please ask your DBA to clear the 'edifact_sent' statu
         total_qty = 0
         # totalHT = 0
         for line_index, order_line in enumerate(order_lines):
-            line = self._build_line(line_index, order_line)
             # totalHT += line['prixUnitaireNet'] * line['montantLigne']
+            product = order_line.product_id
+            uom = product.uom_id
+            line = {
+                'codag': _gv(product, 'code'),
+                'libelle': _gv(product, 'name')[:70],
+                'prixUnitaireNet': int(order_line.price_unit * 10000),
+                'qte': int(order_line.product_qty * 1000),
+                'refarticleFournisseur': _gv(product, 'code'),
+                'ligne': line_index+1,
+                'montantLigne': int(order_line.price_subtotal * 1000),
+                'uq': convert_unit(uom.name),
+                'dateLiv': order_line.date_planned.replace('-', ''),
+                'uep': 1    # CHECKME,
+            }
+
             total_qty += line['qte']
             lines.append(line)
 
@@ -140,43 +156,14 @@ If you need to regenerate, please ask your DBA to clear the 'edifact_sent' statu
     @staticmethod
     def _build_line(line_index, order_line):
         """ build order line """
-        line = {}
-        line['codag'] = order_line.product_id.code
-        line['libelle'] = order_line.product_id.name[:70]
 
-        price_unit = order_line.price_unit
-        line['prixUnitaireNet'] = int(price_unit * 10000)
 
-        # call for behave
-        # La quantité doit tenir compte du conditionnement remonté dans
-        # le champ Quantity du catalogue.
-        #
-        # Si un client saisie 10 en quantité
-        #  sur le site et que le conditionnement présent dans le
-        #  champs quantity = 200,  la quantité sera égal  2000 * 1000'
-        line['qte'] = int(order_line.product_qty * 1000)
-
-        line['refarticleFournisseur'] = order_line.product_id.code
-        line['ligne'] = line_index+1
-
-        montant = int(order_line.price_subtotal * 1000)
-        line['montantLigne'] = montant
-
-        def convert_unit(name):
-            if name == 'PCE':
-                return 'P'
-            assert 0 == 1
-
-        line['uq'] = convert_unit(order_line.product_uom.name)
-        line['dateLiv'] = order_line.date_planned.replace('-', '')
-        line['uep'] = 1    # CHECKME
 
         return line
 
     @classmethod
     def _get_renderer(cls):
         """ get EDIFACT renderer for purchase orders """
-        import tools.edifact
         if cls._edi_renderer is None:
             module_path = os.path.dirname(os.path.abspath(__file__))
             specfile = os.path.join(module_path, 'debonix.json')
