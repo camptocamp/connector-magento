c22c8ac3111b143f860f175c0b8187ad196a1492
CharlineDumontet <charline.dumontet@camptocamp.com>
2017-03-03 16:09:44 +0100

    [Action #8673] Improve delivery file import

--- specific-parts/specific-addons/debonix_purchase_edi/edi_import_tracking.py
+++ specific-parts/specific-addons/debonix_purchase_edi/edi_import_tracking.py
@@ -29,6 +29,7 @@ import csv
 import time
 from datetime import datetime
 from StringIO import StringIO
+import re
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -61,6 +62,7 @@ class EdiImportTracking(orm.Model):
         importpath = company.edifact_purchase_import_path
         successpath = company.edifact_purchase_success_path
         errorpath = company.edifact_purchase_error_path
+        import_file = company.edifact_purchase_import_file
 
         ftp = None
         try:
@@ -68,73 +70,75 @@ class EdiImportTracking(orm.Model):
             # Connect to FTP
             ftp = ftplib.FTP(host, ftpuser, ftppass)
             ftp.cwd(importpath)
+            pattern = re.compile(import_file)
             for filename in ftp.nlst():
-                data = StringIO()
-                ftp.retrbinary('RETR ' + filename, data.write)
+                if pattern.match(filename):
+                    data = StringIO()
+                    ftp.retrbinary('RETR ' + filename, data.write)
 
-                # Go back at the start of the StringIO and read as CSV
-                data.seek(0)
-                csv_data = csv.reader(data, delimiter='|', quotechar='"')
-                for row in csv_data:
-                    po_number = row[3]
-                    date_done = datetime.strptime(row[2], "%Y/%m/%d "
-                                                          "%H:%M:%S.%f")
-                    tracking_ref = row[6].strip() and row[6] or row[0]
-                    if (po_number not in res or
-                            'carrier_tracking_ref' not in res[po_number]):
-                        res[po_number] = {'carrier_tracking_ref': tracking_ref,
-                                          'date_done': date_done}
+                    # Go back at the start of the StringIO and read as CSV
+                    data.seek(0)
+                    csv_data = csv.reader(data, delimiter='|', quotechar='"')
+                    for row in csv_data:
+                        po_number = row[3]
+                        date_done = datetime.strptime(row[2], "%Y/%m/%d "
+                                                              "%H:%M:%S.%f")
+                        tracking_ref = row[6].strip() and row[6] or row[0]
+                        if (po_number not in res or
+                                'carrier_tracking_ref' not in res[po_number]):
+                            res[po_number] = {'carrier_tracking_ref': tracking_ref,
+                                              'date_done': date_done}
+                        else:
+                            res[po_number]['carrier_tracking_ref'] += ";"
+                            res[po_number]['carrier_tracking_ref'] += tracking_ref
+
+                    # Write number in pickings + approve them
+                    with closing(pooler.get_db(cr.dbname).cursor()) as new_cr:
+                        error_message = ""
+                        for po_number in res:
+                            retries = 1
+                            while retries <= 5:
+                                try:
+                                    po_ids = purchase_obj.search(
+                                        new_cr, uid, [('name', '=', po_number)],
+                                        context=context)
+                                    picking_ids = picking_obj.search(
+                                        new_cr, uid, [('purchase_id', 'in',
+                                                       po_ids)],
+                                        context=context)
+                                    picking_obj.write(
+                                        new_cr, uid, picking_ids, res[po_number],
+                                        context=context)
+                                    # Validate picking
+                                    picking_obj.action_move(
+                                        new_cr, uid, picking_ids, context=context)
+                                    new_cr.commit()
+                                    _logger.warning("SOGEDESCA PO %s validated",
+                                                    po_number)
+                                    break
+                                except psycopg2.OperationalError:
+                                    _logger.warning("Failed to validate picking "
+                                                    "for SOGEDESCA PO %s: retry "
+                                                    "%s/5",
+                                                    po_number, retries,
+                                                    exc_info=True)
+                                    retries = retries + 1
+                                    new_cr.rollback()
+                                    time.sleep(2)
+                            if retries == 5:
+                                error_message += "PO %s not imported correctly " \
+                                                 "from filename %s\n" \
+                                                 % (po_number, filename)
+
+                    # Move file to error or success folder
+                    old_filename = importpath + "/" + filename
+                    if error_message:
+                        self.create_error_claim(cr, uid, error_message,
+                                                context=context)
+                        new_filename = errorpath + "/" + filename
                     else:
-                        res[po_number]['carrier_tracking_ref'] += ";"
-                        res[po_number]['carrier_tracking_ref'] += tracking_ref
-
-                # Write number in pickings + approve them
-                with closing(pooler.get_db(cr.dbname).cursor()) as new_cr:
-                    error_message = ""
-                    for po_number in res:
-                        retries = 1
-                        while retries <= 5:
-                            try:
-                                po_ids = purchase_obj.search(
-                                    new_cr, uid, [('name', '=', po_number)],
-                                    context=context)
-                                picking_ids = picking_obj.search(
-                                    new_cr, uid, [('purchase_id', 'in',
-                                                   po_ids)],
-                                    context=context)
-                                picking_obj.write(
-                                    new_cr, uid, picking_ids, res[po_number],
-                                    context=context)
-                                # Validate picking
-                                picking_obj.action_move(
-                                    new_cr, uid, picking_ids, context=context)
-                                new_cr.commit()
-                                _logger.warning("SOGEDESCA PO %s validated",
-                                                po_number)
-                                break
-                            except psycopg2.OperationalError:
-                                _logger.warning("Failed to validate picking "
-                                                "for SOGEDESCA PO %s: retry "
-                                                "%s/5",
-                                                po_number, retries,
-                                                exc_info=True)
-                                retries = retries + 1
-                                new_cr.rollback()
-                                time.sleep(2)
-                        if retries == 5:
-                            error_message += "PO %s not imported correctly " \
-                                             "from filename %s\n" \
-                                             % (po_number, filename)
-
-                # Move file to error or success folder
-                old_filename = importpath + "/" + filename
-                if error_message:
-                    self.create_error_claim(cr, uid, error_message,
-                                            context=context)
-                    new_filename = errorpath + "/" + filename
-                else:
-                    new_filename = successpath + "/" + filename
-                ftp.rename(old_filename, new_filename)
+                        new_filename = successpath + "/" + filename
+                    ftp.rename(old_filename, new_filename)
 
         except (socket.error, IOError) as err:
             raise Warning("Could not retrieve tracking file on FTP server: %s"
