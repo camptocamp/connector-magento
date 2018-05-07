56022cffff1ac83b9477f0c84f91e890ac09d84e
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-05-07 16:51:12 +0200

    BSDEB-1: Create module debonix_supplier_invoice_edi with extra fields

---
+++ specific-parts/specific-addons/debonix_supplier_invoice_edi/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{'name': 'Debonix Supplier Invoice EDI',
+ 'version': '1',
+ 'depends': [
+     'specific_fct'
+ ],
+ 'author': 'Camptocamp SA',
+ 'description': """import supplier invoices from supplier SOGEDESCA""",
+ 'website': 'http://www.camptocamp.com',
+ 'data': [
+     'data/crm_case_categ.xml',
+ ],
+ 'installable': True,
+ }
